106912a1edb6f3c4921ade987bb0331265ce1aee
Anjalikushwaha-23bce10067 <anjali.23bce10067@vitbhopal.ac.in>
2024-08-11 21:29:46 +0530

    Update Activity8.cpp

--- Activity8.cpp
+++ Activity8.cpp
@@ -53,6 +53,15 @@ public:
         return (double)sum / size;
     }
 
+    // Print the vector elements
+    void print() {
+        for (int i = 0; i < size; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
         return false;
     }
 
