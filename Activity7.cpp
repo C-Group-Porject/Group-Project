236166c0efe7d2ad627695a3e4a054735a6caa8f
Shukre Sahil <sahil.23bce10212@vitbhopal.ac.in>
2024-08-13 00:19:17 +0530

    Update Activity7.cpp

--- Activity7.cpp
+++ Activity7.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
 
 struct Book {
     int book_id;
@@ -12,10 +13,10 @@ struct Book {
 };
 
 // Function to read books from a file
-int readBooks(Book books[], int maxBooks, const std::string& filename) {
-    std::ifstream file(filename);
+int readBooks(Book books[], int maxBooks, const string& filename) {
+    ifstream file(filename);
     if (!file) {
-        std::cerr << "Error opening file!" << std::endl;
+        cerr << "Error opening file!" << endl;
         return 0;
     }
 
@@ -24,17 +25,17 @@ int readBooks(Book books[], int maxBooks, const std::string& filename) {
         file.ignore();  // Ignore the space after book_id
 
         // Read the author's name (including spaces) until the comma separator
-        std::getline(file, books[count].author_name, ',');
+        getline(file, books[count].author_name, ',');
 
         // Validate if the author name was read correctly
         if (books[count].author_name.empty()) {
-            std::cerr << "Error reading author name for book " << books[count].book_id << std::endl;
+            cerr << "Error reading author name for book " << books[count].book_id << endl;
             break;
         }
 
         // Read the remaining data
         if (!(file >> books[count].price >> books[count].no_of_pages)) {
-            std::cerr << "Error reading price or number of pages for book " << books[count].book_id << std::endl;
+            cerr << "Error reading price or number of pages for book " << books[count].book_id << endl;
             break;
         }
 
@@ -42,10 +43,10 @@ int readBooks(Book books[], int maxBooks, const std::string& filename) {
         file >> books[count].publisher >> books[count].year_of_publishing;
 
         // Debug output to verify reading process
-        std::cout << "Read book: " << books[count].book_id << " "
+        cout << "Read book: " << books[count].book_id << " "
                   << books[count].author_name << ", " << books[count].price << " "
                   << books[count].no_of_pages << " " << books[count].publisher << " "
-                  << books[count].year_of_publishing << std::endl;
+                  << books[count].year_of_publishing << endl;
 
 count++;
     }
@@ -54,10 +55,10 @@ count++;
 }
 
 // Function to write books to a file
-void writeBooks(const Book books[], int count, const std::string& filename) {
-    std::ofstream file(filename);
+void writeBooks(const Book books[], int count, const string& filename) {
+    ofstream file(filename);
     if (!file) {
-        std::cerr << "Error opening file!" << std::endl;
+        cerr << "Error opening file!" << endl;
         return;
     }
 
@@ -67,7 +68,7 @@ void writeBooks(const Book books[], int count, const std::string& filename) {
              << books[i].price << " "
              << books[i].no_of_pages << " "
              << books[i].publisher << " "
-             << books[i].year_of_publishing << std::endl;
+             << books[i].year_of_publishing << endl;
     }
     file.close();
 }
@@ -94,7 +95,7 @@ int main(){
     int bookCount = readBooks(books, maxBooks, "books.txt");
 
     // Debug output to verify the number of books read
-    std::cout << "Total books read: " << bookCount << std::endl;
+    cout << "Total books read: " << bookCount << endl;
 
     // Sort books by author name
     sortBooksByAuthor(books, bookCount);
@@ -102,7 +103,7 @@ int main(){
     // Write sorted books to output file
     writeBooks(books, bookCount, "sorted_books.txt");
 
-    std::cout << "Books have been sorted and written to sorted_books.txt" << std::endl;
+    cout << "Books have been sorted and written to sorted_books.txt" << endl;
 
     return 0;
 }
