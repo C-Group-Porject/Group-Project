49b032e536fa152723e9e47749b92caa51de3686
Shukre Sahil <sahil.23bce10212@vitbhopal.ac.in>
2024-08-12 19:47:02 +0530

    Update Activity7.cpp

--- Activity7.cpp
+++ Activity7.cpp
@@ -23,5 +23,39 @@ void writeBooks(const Book books[], int count, const std::string& filename) {
     file.close();
 }
 
+// Function to sort books by author name
+void sortBooksByAuthor(Book books[], int count) {
+      for (int i=0; i< count - 1; i++){
+            for(int j = i + 1 ;j < count; j++){
+                  if(books[i].author_name > books[j].author_name){
+                        //Swap books[i] and books[j]
+                        Book temp = books[i];
+                        books[i]=books[j];
+                        books[j]=temp;
+                  }
+            }
+      }
+}
+
+int main(){
+      const int maxBooks = 100;
+    Book books[maxBooks];
+
+    // Read books from file
+    int bookCount = readBooks(books, maxBooks, "books.txt");
+
+    // Debug output to verify the number of books read
+    std::cout << "Total books read: " << bookCount << std::endl;
+
+    // Sort books by author name
+    sortBooksByAuthor(books, bookCount);
+
+    // Write sorted books to output file
+    writeBooks(books, bookCount, "sorted_books.txt");
+
+    std::cout << "Books have been sorted and written to sorted_books.txt" << std::endl;
+
+    return 0;
+}
 
     
